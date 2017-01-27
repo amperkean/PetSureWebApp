872b3c3c10b6ed08c248e9389c2eabc072ff61d3
amperkean <amper_kean@yahoo.com>
2017-01-27 12:25:37 +0800

    little edit

--- petsurweb/app/app.js
+++ petsurweb/app/app.js
@@ -5,20 +5,17 @@
     myControllerFunction.$inject = ["$scope", "$http"];
 
     function myControllerFunction($scope, $http) {
-        $http.get('http://localhost:55434/api/pets/getpets').
+        var url = 'http://localhost:55434/';
+        $http.get( url+'api/pets/getpets').
             success(function (data, status, headers, config) {
-                console.log('asdf');
+                
                 $scope.pets = data;
             }).
             error(function (data, status, headers, config) {
-                console.log('error');
+                
                 console.log(data);
             });
-        $scope.updateText = function () {
-            //$scope.updateLabel = 
-            // use $scope.selectedItem.code and $scope.selectedItem.name here
-            // for other stuff ...
-        }
+ 
     };
 
 })();
